806bcf5efdbd3878fcdaf4cd06fc15c671ff5f6b
aqulu <akku@outlook.com>
2016-04-27 16:42:03 +0200

    implemented event formlar

--- waku-service/public/wakuplnr-app.js
+++ waku-service/public/wakuplnr-app.js
@@ -3,12 +3,22 @@ angular.module('wakuplnr', ['ngResource'])
     $scope.users = UserService.find();
     $scope.events = EventService.find();
     $scope.logme = function() {
-        console.log($scope.startDate + ' - ' + $scope.endDate);
-    }
+        console.log($scope.userid);
+        console.log($scope.day);
+        console.log($scope.startTime + ' - ' + $scope.endTime);
+    };
     
     $scope.saveEvent = function() {
         console.log('implement save event');
-    }
+        var dayParts = $scope.day.split('.');
+        var startTimeParts = $scope.startTime.split(':');
+        var endTimeParts = $scope.endTime.split(':');
+        var start = new Date(dayParts[2], dayParts[1] - 1, dayParts[0], startTimeParts[1], startTimeParts[0]);
+        var end = new Date(dayParts[2], dayParts[1] - 1, dayParts[0], endTimeParts[1], endTimeParts[0]);
+        // TODO generate id
+        var event = {id: '', from: start, to: end, user_id: $scope.userid};
+        EventService.create(event);
+    };
 })
 .service('UserService', function($resource) {
     return $resource('/users/:id', {}, {
@@ -60,6 +70,8 @@ angular.module('wakuplnr', ['ngResource'])
     scope: {
       eventHandler: '&eventHandler',
       value: '=',
+      required: '=',
+      name: '=',
       limitValue: '=limitValue',
       limit: '@limit'
     },
@@ -103,7 +115,9 @@ angular.module('wakuplnr', ['ngResource'])
     templateUrl: 'timepicker.html',
     scope: {
       eventHandler: '&eventHandler',
-      value: '='
+      value: '=',
+      required: '=',
+      name: '='
     },
 
     link: function(scope, element, attrs){
