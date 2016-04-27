d93caedab5fc6c9bd736e468809d5dac6cf48cd1
aqulu <akku@outlook.com>
2016-04-27 15:04:14 +0200

    added datepicker to events.html

--- waku-service/public/wakuplnr-app.js
+++ waku-service/public/wakuplnr-app.js
@@ -2,6 +2,13 @@ angular.module('wakuplnr', ['ngResource'])
 .controller('MainCtrl', function($scope, UserService, EventService) {
     $scope.users = UserService.find();
     $scope.events = EventService.find();
+    $scope.logme = function() {
+        console.log($scope.startDate + ' - ' + $scope.endDate);
+    }
+    
+    $scope.saveEvent = function() {
+        console.log('implement save event');
+    }
 })
 .service('UserService', function($resource) {
     return $resource('/users/:id', {}, {
@@ -45,4 +52,49 @@ angular.module('wakuplnr', ['ngResource'])
             }
         }
     });
+})
+.directive('wakuDate', function($timeout){
+  return {
+    restrict: 'E',
+    templateUrl: 'datepicker.html',
+    scope: {
+      eventHandler: '&eventHandler',
+      value: '=',
+      limitValue: '=limitValue',
+      limit: '@limit'
+    },
+
+    link: function(scope, element, attrs){
+      element.find('.input-group.date').datepicker({
+        format: "dd.mm.yyyy",
+        "todayBtn": "linked",
+        todayHighlight: true,
+        "autoclose": true
+      });
+
+      element.find('.input-group.date').datepicker('update', scope.value);
+
+      scope.valueChanged = function(){
+        $timeout(function() {
+          scope.eventHandler();
+        }, 1);
+      };
+      scope.$watch('limitValue', function(val){
+
+        if(scope.limit === 'endDate'){
+          element.find('.input-group.date').datepicker('setEndDate', val);
+        }
+
+        if(scope.limit === 'startDate'){
+          element.find('.input-group.date').datepicker('setStartDate', val);
+        }
+
+      });
+
+      scope.openDatepicker = function(){
+        element.find('.input-group.date').datepicker('show');
+      };
+    }
+  };
+
 });
