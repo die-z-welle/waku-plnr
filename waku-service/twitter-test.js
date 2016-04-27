75457cc4ae9f800f28278a21d411f28b94df8169
aqulu <akku@outlook.com>
2016-04-27 23:50:02 +0200

    twitter direct_message example

--- waku-service/twitter-test.js
+++ waku-service/twitter-test.js
@@ -2,14 +2,19 @@ var Twit = require('twit')
 
 var T = new Twit({
   consumer_key:         'XYBOqj4WHHHLTN03H8xI7yteJ',
-  // don't save the secrets anywhere on github!
+  // don't save this on github!
   consumer_secret:      '...',
   access_token:         '725436727637213184-YkMfLMlOuyhEbMoJZ9XMa6reaG1P1kU',
-  // don't save the secrets anywhere on github!
+  // don't save this on github!
   access_token_secret:  '...',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//  console.log(data)
+//});
+
+T.post('direct_messages/new', { screen_name: 'aqulu1', text: 'hallo test 1234' }, function(err, data, response) {
   console.log(data)
 });
+
