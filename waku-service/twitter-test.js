9034e5ae437bd4b254f379feafeee07ca4f9b4ad
aqulu <akku@outlook.com>
2016-05-01 17:03:06 +0200

    changed twitter-test (store credentials locally)

--- waku-service/twitter-test.js
+++ waku-service/twitter-test.js
@@ -1,14 +1,15 @@
 var Twit = require('twit')
-
-var T = new Twit({
-  consumer_key:         'XYBOqj4WHHHLTN03H8xI7yteJ',
+var credentials = require('../../credentials.json');
+var T = new Twit(credentials);
+//var T = new Twit({
+//  consumer_key:         'XYBOqj4WHHHLTN03H8xI7yteJ',
   // don't save this on github!
-  consumer_secret:      '...',
-  access_token:         '725436727637213184-YkMfLMlOuyhEbMoJZ9XMa6reaG1P1kU',
+//  consumer_secret:      '...',
+//  access_token:         '725436727637213184-YkMfLMlOuyhEbMoJZ9XMa6reaG1P1kU',
   // don't save this on github!
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+//  access_token_secret:  '...',
+//  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+//});
 
 //T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
 //  console.log(data)
