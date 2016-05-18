32d0114d78505e7cebb255160c29abe5b982a9e2
Valentin Meier <meier.valentin@gmail.com>
2016-05-18 15:59:17 +0200

    Added function to calculate average over last 10 values / Added threshold sensitivity

--- sensor/arduino/sensorvalue_plotter/sensorvalue_plotter.ino
+++ sensor/arduino/sensorvalue_plotter/sensorvalue_plotter.ino
@@ -1,16 +1,48 @@
-#include "EmonLib.h"                   // Include Emon Library
+#include "EmonLib.h"
 EnergyMonitor emon1;
 
+const int gpioPin = 8;
+const double irmsTreshold = 0.2; //In Percent!
+const int numReadings = 10;
+bool isOn = false;
+double readings[10];
+int readIndex = 0;
+double total = 0;
+double average = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   emon1.current(1, 111.1); 
+  pinMode(gpioPin, OUTPUT);
+}
+
+void calculateAverage(double value) {
+  total = total - readings[readIndex];
+  readings[readIndex] = value;
+
+  total = total + readings[readIndex];
+
+  readIndex = readIndex + 1;
+
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+
+  average = total / numReadings;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-  Serial.print(Irms*230.0);         // Apparent power
-  Serial.print(" ");
-  Serial.println(Irms);          // Irms
+  double irms = emon1.calcIrms(1480);
+  calculateAverage(irms);
+  Serial.println(irms); //Remove when done
+  
+  if(irms > (average + average * irmsTreshold) && !isOn) {
+       isOn = true;
+       digitalWrite(gpioPin, HIGH);
+       Serial.println("Waschmaschiene AN");
+  } else if(irms < (average - average * irmsTreshold) && isOn) {
+    digitalWrite(gpioPin, LOW);
+    isOn = false;
+    Serial.println("Waschmaschiene AUS");
+  }
 }
