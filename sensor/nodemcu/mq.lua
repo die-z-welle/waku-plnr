370ca1386812baf4034b8678b6104acafe264ff2
aqulu <akku@outlook.com>
2016-05-30 10:12:08 +0200

    queue changes; start timer on mq connect

--- sensor/nodemcu/mq.lua
+++ sensor/nodemcu/mq.lua
@@ -10,7 +10,7 @@ topic = "waku"
 -- gpio config
 pin = 1  -- D1
 
-createJson = function(state)
+createJson = function(status)
   return ('{"_id": "' .. id .. '", "location": "", "state": "'.. state .. '"}')
 end
 
@@ -24,24 +24,31 @@ m = mqtt.Client(chipid, 120, "", "")
 m:lwt("/lwt", "offline", 0, 0)
 m:on("connect", function(client) print ("connected") end)
 m:on("offline", function(client) print ("offline") end)
-m:connect(ip, port, secure, function(client) print("connected") end,
-                            function(client, reason) print("failed reason: " .. reason) end)
-m:subscribe(topic, 0, function(client) print("subscribe success") end)
 
-gpio.mode(pin, gpio.INPUT)
-oldVal = 0
---automatically repeat alarm every 2000ms
-tmr.alarm(1, 2000,1,function()
-  newVal = gpio.read(pin)
-  if (newVal ~= oldVal) then
-    if (newVal == 1) then
-      state = "washing"
-    else
-      state = "idle"
+m:connect(ip, port, secure,
+    function(client)
+        print("connected")
+        gpio.mode(pin, gpio.INPUT)
+        oldVal = 0
+        --automatically repeat alarm every 2000ms
+        tmr.alarm(1, 2000,1,function()
+          print("alarm")
+          newVal = gpio.read(pin)
+          if (newVal ~= oldVal) then
+            if (newVal == 1) then
+              state = "washing"
+            else
+              state = "idle"
+            end
+            m:publish(topic, createJson(state), 0, 0, function(client) print("sent state") end)
+          end
+          oldVal = newVal
+        end)
+
+    end,
+    function(client, reason)
+        print("failed reason: " .. reason)
     end
-    m:publish(topic, createJson(state), 0, 0, function(client) print("sent state") end)
-  end
-  oldVal = newVal
-end)
--- m:close();
--- you can call m:connect again
+)
+
+m:subscribe(topic, 0, function(client) print("subscribe success") end)
